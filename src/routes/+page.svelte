9ffdcbc5a74ab3345fe5ae1a674c361736ed051f
Philemon Wachara <pwachara@hotmail.com>
2026-02-17 14:30:13 +0300

    another change to the UI

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,3 @@
 <h1 class="text-blue-500 font-bold text-3xl">Welcome to Pocketbase</h1>
 <p>This application has been extended using Golang</p>
-<p class="flex w-full text-gray-700 font-bold">Here is a change. Check again if database is persisted and this change is displayed.</p>
+<p class="flex w-full text-gray-700 font-bold">Here is another change. Check again if database is persisted.</p>
