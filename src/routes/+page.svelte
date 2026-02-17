435564d1e4354383acd641c231139bed2828f24c
Philemon Wachara <pwachara@hotmail.com>
2026-02-17 10:06:43 +0300

    updated landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,3 @@
 <h1 class="text-blue-500 font-bold text-3xl">Welcome to Pocketbase</h1>
 <p>This application has been extended using Golang</p>
-<p class="flex justify-center items-center w-full text-red-500 font-bold">Here is a change. Does it propagate to production?</p>
+<p class="flex w-full text-gray-700 font-bold">Here is a change. Check if database is persisted.</p>
