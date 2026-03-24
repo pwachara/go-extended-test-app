f0e6fc229c461d3fb1d29f7015cf0a77cbc2220e
Philemon Wachara <pwachara@hotmail.com>
2026-03-24 10:40:34 +0300

    added update through github

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,4 @@
 <h1 class="text-blue-500 font-bold text-3xl">Welcome to Pocketbase</h1>
 <p>This application has been extended using Golang</p>
 <p class="flex w-full text-gray-700 font-bold">Here is another change. Check again if database is persisted.</p>
+<p class="font-bold">Last Change through Github?</p>
